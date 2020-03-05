ca8ac9893fe9280e8355f34b56ad3dd25e934323
Alban ON <34128056+Sasorishi@users.noreply.github.com>
2020-03-05 14:21:50 +0100

    Init project

---
+++ cloudlife/src/main/java/sasorishi/cloudlife/CloudlifeApplication.java
@@ -0,0 +1,13 @@
+package sasorishi.cloudlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudlifeApplication.class, args);
+	}
+
+}
---
+++ cloudlife/src/test/java/sasorishi/cloudlife/CloudlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package sasorishi.cloudlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
